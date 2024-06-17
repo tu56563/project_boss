75a2b4ec2c96b6134e1283e170d47494d3e0d623
jslee <165612351+tu56563@users.noreply.github.com>
2024-06-17 12:51:20 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,4 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.getElementById('burger-menu');
+    const leftMenu = document.querySelector('header .left_menu');
+    const fixBg = document.querySelector('header .fix_bg');
+    const closeBtn = document.querySelector('header .left_menu .close');
+    const body = document.body;
+
+    burgerMenu.addEventListener('click', function(event) {
+        event.preventDefault();
+        fixBg.style.display = 'block';
+        fixBg.style.zIndex = '999'; // fixBg 요소의 z-index 설정
+        leftMenu.classList.add('active');
+        body.style.overflow = 'hidden'; // 스크롤 비활성화
+        setTimeout(() => {
+            leftMenu.style.transform = 'translateX(300px)';
+        }, 10);
+    });
+
+    closeBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        leftMenu.style.transform = 'translateX(-300px)';
+        fixBg.style.opacity = '0';
+        setTimeout(() => {
+            leftMenu.classList.remove('active');
+            fixBg.style.display = 'none';
+            fixBg.style.opacity = '0.5';
+            body.style.overflow = ''; // 스크롤 활성화
+        }, 500);
+    });
+});
+// =================================
+document.addEventListener('DOMContentLoaded', function() {
     const menuItems = document.querySelectorAll('nav .gnb .title');
 
     menuItems.forEach(item => {
